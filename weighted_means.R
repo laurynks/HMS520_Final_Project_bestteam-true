6c2a3a616bdf950df1116877c0bb629af64b42c0
Jessica Bishai <jbishai@ihme.washington.edu>
2020-11-23 00:05:51 -0500

    first few edits to weighted_means

--- weighted_means.R
+++ weighted_means.R
@@ -1,4 +1,9 @@
-# this is copied and pasted from the specific case I had to do it for.
+################################################################################
+# HMS 520
+# Final Project
+# Weighted Mean Function
+# Authors: Lauryn Stafford and Jessica Bishai
+################################################################################
 
 source("/ihme/cc_resources/libraries/current/r/get_outputs.R")
 source("/ihme/cc_resources/libraries/current/r/get_ids.R")
@@ -9,6 +14,36 @@ source("/ihme/cc_resources/libraries/current/r/get_location_metadata.R")
 
 library(dplyr)
 library(ggplot2)
+
+weighted_mean <- function(df) {
+  
+  # retrieve IHME population data
+  pop <- get_population(
+    # pull in the same specs that the input data has.
+    age_group_id = unique(df$age_group_id),
+    year_id = unique(df$age_group_id),
+    sex_id = unique(df$sex_id),
+    location_id = unique(df$location_id),
+    # Assume decomp_step is always iterative
+    decomp_step = 'iterative'
+  )
+  
+  # calculate a weighted mean by age of inpatient utilization
+  df_pop <- merge(df, pop, by = c('age_group_id', 'year_id', 'sex_id', 
+                                  'location_id'))
+  
+  # THIS LINE NEEDS TO BE REWORKED; look at data.table suggestion from Will
+  weighted_df <- df_pop %>% group_by(location_id) %>% mutate(pop_share = population/sum(population)) %>% 
+    mutate(weighted_mean_share = pop_share * mean) %>% summarise(weighted_mean = sum(weighted_mean_share))
+
+  #weighted_utils$mean_utils <- weighted_utils$weighted_mean
+  
+  return(weighted_df)
+}
+
+
+
+# Test the function
 
 # get in-facility delivery data
 IFD <- get_covariate_estimates(
@@ -39,17 +74,3 @@ pop <- get_population(
   location_id = unique(util$location_id), # pull in the same location_ids used in the utilization
   decomp_step = 'iterative'
 )
-
-# calculate a weighted mean by age of inpatient utilization
-util_pop <- merge(util, pop, by = c('age_group_id', 'location_id'))
-weighted_utils <- util_pop %>% group_by(location_id) %>% mutate(pop_share = population/sum(population)) %>% 
-  mutate(weighted_mean_share = pop_share * mean) %>% summarise(weighted_mean = sum(weighted_mean_share))
-
-weighted_utils$mean_utils <- weighted_utils$weighted_mean
-
-# merge data
-full_dat <- merge(IFD, weighted_utils, by='location_id')
-
-loc_data <- get_location_metadata(location_set_id=22, gbd_round_id=7)
-
-full_dat_locs <- merge(full_dat, loc_data, by = 'location_id', all.x = TRUE)
