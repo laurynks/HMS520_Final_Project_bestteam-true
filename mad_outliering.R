d332c7dc51489742c58c212300b80ac82eb2fa9e
Lauryn Stafford <laurynks@ihme.washington.edu>
2020-11-29 21:37:35 -0800

    cleaner mad outliering script

--- mad_outliering.R
+++ mad_outliering.R
@@ -1,8 +1,13 @@
-#MAD OUTLIERING  -----------------------------------------------------------------------------------------------------------------------------------------
-#this needs to be reconfigured w/ less comments and the outliering as a function
-##working environment
+####################################################
+## Original Author: Phoebe Rhinehart
+## Modified by: Lauryn Stafford and Jessica Bishai
+## Date: 11/25/20
+## Description: MAD Outliering
+####################################################
+
 rm(list=ls())
 
+# working environment
 if (Sys.info()[1] == "Linux"){
   j_root <- "/snfs1/"
   h_root <- "~/"
@@ -10,127 +15,8 @@ if (Sys.info()[1] == "Linux"){
   j_root <- "J:/"
   h_root <- "H:/"
 }
-## Source central functions
-source("/ihme/cc_resources/libraries/current/r/get_age_metadata.R")
-source("/ihme/cc_resources/libraries/current/r/get_bundle_version.R")
-source("/ihme/cc_resources/libraries/current/r/get_bundle_data.R")
-source("/ihme/cc_resources/libraries/current/r/get_crosswalk_version.R")
-source("/ihme/cc_resources/libraries/current/r/save_crosswalk_version.R")
-source("/ihme/cc_resources/libraries/current/r/get_age_metadata.R")
-source("/ihme/cc_resources/libraries/current/r/get_ids.R")
-library("data.table")
-library("ggplot2")
-library("readr")
-library("RMySQL")
-library("openxlsx")
-library("readxl")
-library("stringr")
-library("tidyr")
-library("plyr")
-library("dplyr")
-## SET OBJETCS
-b_id <- 217
-out_dir <- paste0(j_root, "WORK/12_bundle/msk_pain_lowback/", b_id, "/03_review/02_upload/")
-date <- gsub("-", "_", Sys.Date())
-bv_id <-  8996
-cv_id <- 4892
-mrbrt_dir <- paste0(j_root, "temp/prhine22/MrBeRT/lbp/")
-## GET DATA
-cw_data <- get_crosswalk_version(cv_id, export = F) #use cvid used to run current model in epiviz that you showed theo
-cw_out <- cw_data[ ,is_outlier := NA] #start over on the outliering
-cw_mad <- copy(cw_out)
-byvars <- c("location_id", "year_start", "year_end", "nid")
-## GET AGE WEIGHTS
-ages <- as.data.table(get_age_metadata(age_group_set_id=12)) #double check what this age group id coresponds too
-setnames(ages, old = c("age_group_years_start", "age_group_years_end"), new = c("age_start", "age_end"))
-ages[age_start >= 1 , age_end := age_end - 1]
-all_ages <- copy(ages)
-n <- 2 #set the number of deviations to outlier at
-#DO THIS TO CW MAD
-dt <- copy(cw_mad) #going to do all outliering at once
-dt_out <- merge(dt, all_ages, by = c("age_start", "age_end"), all.x = T) ##merge age table map and merge on to dataset
-#calculate age-standardized prevalence/incidence
-##create new age-weights for each data source
-dt_out[, sum := sum(age_group_weight_value), by = byvars] #sum of standard age-weights for all the ages we are using, by location-age-sex-nid, sum will be same for all age-groups and possibly less than one
-dt_out[, new_weight := age_group_weight_value/sum, by = byvars] #divide each age-group's standard weight by the sum of all the weights in their locaiton-age-sex-nid group
-##age standardizing per location-year by sex
-#add a column titled "age_std_mean" with the age-standardized mean for the location-year-sex-nid
-dt_out[, as_mean := mean * new_weight] #initially just the weighted mean for that AGE-location-year-sex-nid
-dt_out[, as_mean := sum(as_mean), by = byvars] #sum across ages within the location-year-sex-nid group, you now have age-standardized mean for that series
-##mark as outlier if age standardized mean is 0 (either biased data or rare disease in small ppln)
-dt_out[as_mean == 0, `:=` (is_outlier = 1, note_modeler = "outliered this location-year-sex-NID-cdType age-series because age standardized mean is 0" )]
-## log-transform to pick up low outliers
-dt_out[as_mean != 0, as_mean := log(as_mean)] #outliering occurs using the 'as_mean' variable, which is the age_standardized mean for each series
-# calculate MAD (median of the residuals from the data's true median)
-dt_out[as_mean == 0, as_mean := NA] # don't count zeros in median calculations
-dt_out[,median:=median(as_mean,na.rm = T),by=c("sex")] #unsure why the median is being done by sex and not by the "by_vars" (b/c we are outliering based on each series mad)
-dt_out[,mad:=mad(as_mean,na.rm = T),by=c("sex")]
-#***can change number of MAD to mark here
-dt_outlier <- dt_out[as_mean > ((n*mad)+median), `:=` (is_outlier = 1,
-                                                       note_modeler = paste0(note_modeler, " | outliered because log age-standardized mean for location-year-sex-NID is higher than 2 MAD above median"))]
-dt_outlier <- dt_outlier[as_mean < (median - (n*mad)), `:=` (is_outlier = 1,
-                                                             note_modeler = paste0(note_modeler, " | outliered because log age-standardized mean for location-year-sex-NID is lower than 2 MAD below median"))]
-# dt_outlier[mean > 0.1 & is_outlier %in% c(0, NA), is_outlier := 1]
-dt_outlier[is.na(is_outlier), is_outlier := 0]
-print(paste(nrow(dt_outlier[is_outlier == 1]), "points were outliered with", n, "MAD"))
-percent_outliered <- round((nrow(dt_outlier[is_outlier == 1]) / nrow(dt_outlier))*100, digits = 1)
-print(paste("outliered", percent_outliered, "% of data"))
-dropped_locs <- setdiff(unique(cw_data$country), unique(dt_outlier[is_outlier==0]$country))
-print(paste("Dropped ", length(dropped_locs), " countries from model:", paste(dropped_locs, collapse = " ")))
-dt_outlier[, c("sum", "new_weight", "as_mean", "median", "mad", "age_group_weight_value", "age_group_id") := NULL]
-# dt_final <- rbind(cw_nomad, dt_out, fill=TRUE)
-dt_final <- copy(dt_outlier)
 
-version <- get_bundle_version(bv_id, export = F)
-vers_seqs <- unique(version$seq)
-del_seqs <- setdiff(unique(dt_final$seq), vers_seqs)
-dt_final[seq %in% del_seqs, seq := ""]
-
-dt_final[location_name == "Shanghai", is_outlier := 1] #NECK PAIN ONLY
-
-path <- paste0(out_dir, "cwv_MAD2_outliering.xlsx")
-write.xlsx(dt_final, path, sheetName = "extraction")
-
-outlier_fpath <-  path
-description <- paste("outliered data 2 MAD above/below median (", percent_outliered, "%),", paste("dropped", length(dropped_locs), "countries from model:", paste(dropped_locs, collapse = " ")))
-
-xw_result <- save_crosswalk_version(
-  bundle_version_id = bv_id,
-  data_filepath=outlier_fpath,
-  description=description)
-# GRAPH -----------------------------------------------------------------------------------------------------------------------------------
-# Prep data for plotting
-graph_dt <- copy(dt_final)
-graph_dt[, age_mid := (age_start + age_end)/2]
-graph_dt[, age_group := paste0(age_start, "-", age_end)]
-outlier_plot <- ggplot(graph_dt, aes(x = age_mid, y = mean, color = as.factor(is_outlier))) +
-  facet_wrap(~clinical_data_type) +
-  geom_point(alpha = 0.4) + theme_bw() +
-  ggtitle(paste0("LBP - Outlier MAD > ", n)) + xlab("Age Group") + ylab("Mean")
-print(outlier_plot)
-ggsave(paste0(mrbrt_dir, "mad_trim_plot.pdf"))
-
-
-##################################
-## STEP 4 MAD OUTLIERING PROCESS
-##################################
-
-# SOURCE AND OBJECTS ----------------------------------------------------------
-rm(list=ls())
-##working environment
-os <- .Platform$OS.type
-if (os=="windows") {
-  lib_path <- "C:/Users/hhan5/Documents/R/win-library/3.4"
-  j_root<- "J:/"
-  h_root<-"H:/"
-  scratch <-"H:/outliering"
-} else {
-  lib_path <- "temp/central_comp/libraries/current/r/"
-  j_root<- "/home/j/"
-  h_root<-"/homes/bsheena/"
-  scratch <- "/share/scratch/users/bsheena/outliering"
-}
-#set object
+# set object
 functions_dir <- paste0(j_root, "temp/central_comp/libraries/gbd_env/r/")
 temp_dir <- paste0(j_root, "temp/prhine22/")
 date <- Sys.Date()
@@ -139,8 +25,9 @@ bv_id <- 15335 # new bundle version id
 step4_cv_id <- 11054 # crosswalked data that you need to do MAD outliering on
 b_id <- 6926
 cause <- "msk_rheumarthritis"
+n <- 2 # this will be the number of MAD you will outlier
 
-## Source central functions
+# source central functions
 source("/ihme/cc_resources/libraries/current/r/get_age_metadata.R")
 source("/ihme/cc_resources/libraries/current/r/get_bundle_version.R")
 source("/ihme/cc_resources/libraries/current/r/get_crosswalk_version.R")
@@ -157,10 +44,8 @@ library("stringr")
 library("tidyr")
 library("plyr")
 library("dplyr")
-## SET OBJECTS
-n <- 2 # this will be the number of MAD you will outlier
-## GET DATA
-#
+
+## get data 
 original_data4 <- get_crosswalk_version(step4_cv_id)
 dt_emr <- original_data4[measure == "mtexcess", ]
 dt_other_measures <- original_data4[measure != "prevalence" & measure != "mtexcess"]
@@ -170,7 +55,7 @@ cv_drop <- c("cv_survey", "cv_hospital", "cv_literature", "cv_marketscan_all_200
              "cv_marketscan_all_2010", "cv_marketscan_inp_2010", "cv_marketscan_all_2012", "cv_marketscan_inp_2012", "cv_inpatient")
 byvars <- c("location_id", "sex", "year_start", "year_end", "nid")
 
-## GET AGE WEIGHTS
+## get age weights
 all_fine_ages <- as.data.table(get_age_metadata(age_group_set_id=12))
 not_babies <- all_fine_ages[!age_group_id %in% c(2:4)]
 not_babies[, age_end := age_group_years_end-1]
@@ -182,7 +67,7 @@ all_ages[, c("age_group_years_start", "age_group_years_end") := NULL]
 all_ages <- rbind(all_ages, all_ages[20,])
 all_ages <- all_ages[1:21,]
 
-## SET UP DATATABLE
+## set up datatable
 dt<- dt[!is.na(mean)]
 dt_merged <- merge(dt, all_ages, by = c("age_start", "age_end"), all.x = T) # merge age table map and merge on to dataset
 
@@ -194,17 +79,17 @@ for (i in 1:nrow(dt_merged)) {
   }
 }
 
-#calculate age-standardized prevalence/incidence
-
-##create new age-weights for each data source
+# calculate age-standardized prevalence/incidence
+## create new age-weights for each data source
 dt_merged <- dt_merged[, sum := sum(age_group_weight_value), by = byvars] #sum of standard age-weights for all the ages we are using, by location-age-sex-nid, sum will be same for all age-groups and possibly less than one
 dt_merged <- dt_merged[, new_weight := age_group_weight_value/sum, by = byvars] #divide each age-group's standard weight by the sum of all the weights in their locaiton-age-sex-nid group
-##age standardizing per location-year by sex
-#add a column titled "age_std_mean" with the age-standardized mean for the location-year-sex-nid
+
+## age standardizing per location-year by sex
+# add a column titled "age_std_mean" with the age-standardized mean for the location-year-sex-nid
 dt_merged[, as_mean := mean * new_weight] #initially just the weighted mean for that AGE-location-year-sex-nid
 dt_merged[, as_mean := sum(as_mean), by = byvars] #sum across ages within the location-year-sex-nid group, you now have age-standardized mean for that series
 
-##mark as outlier if age standardized mean is 0 (either biased data or rare disease in small ppln)
+## mark as outlier if age standardized mean is 0 (either biased data or rare disease in small ppln)
 dt_merged[as_mean == 0, is_outlier_new := 1]
 dt_merged[as_mean == 0, note_modeler := paste0(note_modeler, " | outliered this location-year-sex-NID age-series because age standardized mean is 0 ", date)]
 
@@ -228,8 +113,9 @@ print(paste("outliered", percent_outliered, "% of data"))
 dropped_locs <- setdiff(unique(original_data4$country), unique(dt_merged[is.na(is_outlier_new)]$country))
 print(paste("Dropped ", length(dropped_locs), " countries from model:", paste(dropped_locs, collapse = " ")))
 
-## SEND TO REVIEW FOLDER
+## send to review folder
 outlier_dt <- copy(dt_merged)
+
 # Situations where the outlier status would change
 # UN-OUTLIER
 outlier_dt[is_outlier == 1 & is.na(is_outlier_new) & grepl("MAD", note_modeler),
