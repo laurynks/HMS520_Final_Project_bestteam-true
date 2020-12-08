8f881c885a7bcf327f0f544d5c3f9bb142da103a
Jessica Bishai <jbishai@ihme.washington.edu>
2020-12-07 22:56:26 -0500

    start to load sample functions. note STGPR doesn't work

---
+++ bundle_converter.R
@@ -0,0 +1,14 @@
+################################################################################
+# HMS 520 
+# Bundle Converter
+# Authors: Lauryn Stafford and Jessica Bishai
+################################################################################
+source("/ihme/cc_resources/libraries/current/r/get_bundle_data.R")
+source("/ihme/cc_resources/libraries/current/r/get_crosswalk_version.R")
+
+# pull in sample DisMod bundle
+sample_DisMod <- get_bundle_data(bundle_id = 435, 
+                                 decomp_step = "iterative",
+                                 gbd_round_id = 7)
+
+sample_STGPR <- get_crosswalk_version(crosswalk_version_id = 31258)
